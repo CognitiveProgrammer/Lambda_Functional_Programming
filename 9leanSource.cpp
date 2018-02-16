36992a54c493c737ab95dbe6c5e314230cfa0b24
@9lean <9lean@users.noreply.github.com>
2018-02-16 20:21:52 +0530

    Update 9leanSource.cpp

--- 9leanSource.cpp
+++ 9leanSource.cpp
@@ -11,6 +11,7 @@
 * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
 * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+// visit https://github.com/9lean/Lambda_Functional_Programming for code description
 
 #include <iostream>
 #include <functional>
